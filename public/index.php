e9552ff3d81177c488ce8699d179e9a6272faa57
jakecleary <shout@jakecleary.net>
2015-12-07 00:18:07 +0000

    Improve the entry file

--- public/index.php
+++ public/index.php
@@ -1,12 +1,22 @@
 <?hh
 
 use Hakkit\Core\Http\Request;
+use Hakkit\Core\Http\Response;
+use Hakkit\Core\Router\Router;
+use Hakkit\Core\Container\Application;
 
 define('APP_ROOT', realpath(__DIR__ . '/../'));
 require_once APP_ROOT . '/vendor/autoload.php';
 
-function main() : void {
-	$request = Request::constructFromGlobals();
+function main() : void 
+{
+	$app = new Application(
+		new Router(require_once APP_ROOT . '/appl/routes.php')
+	);
+	
+	echo $app->handle(
+		Request::constructFromGlobals()
+	);
 }
 
 main();
